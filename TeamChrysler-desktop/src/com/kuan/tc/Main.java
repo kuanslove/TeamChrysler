dc1b6d745d5e748e288e2c130cdfa148b30bbc0e
kuanslove <kuanslove@hotmail.com>
2013-10-06 15:38:59 -0400

    setup project TeamChrysler

---
+++ TeamChrysler-desktop/src/com/kuan/tc/Main.java
@@ -0,0 +1,16 @@
+package com.kuan.tc;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "TeamChrysler";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new TC(), cfg);
+	}
+}
